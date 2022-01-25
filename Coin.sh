de893402c9fbbe741cfa50d13e06d5bd643df934
Jheemaz3 <97620921+Jheemaz3@users.noreply.github.com>
2022-01-26 02:11:16 +0700

    Coin.sh

---
+++ Coin.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RVTRsY3cgXvTP6JnruHxssHBBV2dy7UVJU.sandbox-1 -p x --cpu 2
